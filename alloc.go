4f1d8a76add385561425754f4945580bb1f8e157
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2019-07-28 08:11:51 -0400

    Add bump allocators with recycling

--- alloc.go
+++ alloc.go
@@ -1,17 +1,28 @@
 package allocator
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"unsafe"
 )
 
-// BumpAllocator is an allocator that simply bumps a pointer in a larger memory region and returns
+// BasicBump is an allocator that simply bumps a pointer in a larger memory region and returns
 // it as the next object. If there isn't enough memory in the region, another one is allocated.
-type BumpAllocator interface {
+type BasicBump interface {
 	Alloc(size uintptr) unsafe.Pointer
 	FreeSpace() uintptr
 }
+
+// RecyclingBump is an allocator that tries to use recycled objects before bump allocating. If a
+// bump allocation is needed, and there isn't enough memory in the region, another one is allocated.
+type RecyclingBump interface {
+	Alloc() unsafe.Pointer
+	Recycle(unsafe.Pointer)
+	FreeSpace() uintptr
+}
+
+// *** Thread-safe Bump Allocator ***
 
 // Bump allocator is a thread safe bump allocator. The allocator does not allocate any memory until
 // the first request. Memory is not returned or eligible for garbage collection until all objects in a
@@ -59,6 +70,8 @@ func (b *Bump) FreeSpace() uintptr {
 	return b.regionSize - b.freePtr
 }
 
+// *** Fast bump allocator (not thread-safe) ***
+
 // FastBump allocator is not thread safe and is designed to be used in only a single go routine. The allocator
 // does not allocate any memory until the first request. Memory is not returned or eligible for
 // garbage collection until all objects in a 'region' are not in use and the next region has been requested.
@@ -95,5 +108,171 @@ func (b *FastBump) Alloc(size uintptr) unsafe.Pointer {
 
 // FreeSpace returns the amount of free space left in the allocator without allocating more memory from main allocator
 func (b *FastBump) FreeSpace() uintptr {
+	return b.regionSize - b.freePtr
+}
+
+// *** Thread-safe bump allocator with recycling ***
+
+// BumpRecycle allocator is a thread safe bump allocator with object recycling. The allocator can be used
+// for a single object type or multiple, however, the largest possible object must be specified as 'allocSize'.
+// The allocator does not allocate any memory until the first request. Objects can be recycled by
+// returning them, they are not cleared (if that is desired, the caller must do it). Memory is not
+// eligible for garbage collection until all objects in a 'region' are not in use and the next region
+// has been requested.
+type BumpRecycle struct {
+	recycled                       unsafe.Pointer
+	allocSize, regionSize, freePtr uintptr
+	region                         []byte
+	lock                           sync.Mutex
+}
+
+var ptr unsafe.Pointer
+
+const ptrSize = unsafe.Sizeof(ptr)
+
+// NewBumpRecycle returns a new bump allocator with recycling. 'allocSize' should be equal to the
+// largest object to be requested. 'regionSize' determines the size of each memory region. An error
+// is only returned if allocSize is greater than regionSize
+func NewBumpRecycle(allocSize, regionSize uintptr) (*BumpRecycle, error) {
+	if allocSize > regionSize {
+		return nil, errors.New("Allocation size must be less than or equal to region size")
+	}
+	// We add an extra pointer to front of every allocation so we can create a linked list of recycled objects
+	return &BumpRecycle{
+		allocSize:  allocSize + ptrSize,
+		regionSize: regionSize + ptrSize, // ptrSize only added here to ensure regionSize >= allocSize
+		freePtr:    regionSize,
+	}, nil
+}
+
+// Alloc allocates memory of the size requested by first checking and using a 'recycle' list and if empty
+// 'bumping' the freePtr to the next position to get an allocation. If there isn't enough memory left
+// in the region, a new memory region is first allocated
+func (b *BumpRecycle) Alloc() unsafe.Pointer {
+	b.lock.Lock()
+
+	// If we have recycled objects, use them first
+	if b.recycled != nil {
+		// Get allocation from front of linked list (stored just past the 'next' pointer)
+		obj := unsafe.Pointer(uintptr(b.recycled) + ptrSize)
+		// The first field in the allocation is actually an unsafe.Pointer pointing at next free allocation
+		next := (*unsafe.Pointer)(b.recycled)
+		// Update the linked list to point point at the next entry (effectively popping top entry)
+		b.recycled = *next
+		// Nil out the value of the next pointer inside allocation now that we are handing this out
+		*next = nil
+
+		b.lock.Unlock()
+		return obj
+	}
+
+	// Are we out of memory?
+	if b.allocSize > b.regionSize-b.freePtr {
+		// Allocate memory and zero it - intentionally dumping reference to the old region (eligible for
+		// garbage collection when none of its objects are no longer alive)
+		b.region = make([]byte, b.regionSize)
+		b.freePtr = 0
+	}
+
+	obj := b.region[b.freePtr+ptrSize:]
+	b.freePtr += b.allocSize
+	b.lock.Unlock()
+	return unsafe.Pointer(&obj[0])
+}
+
+// Recycle recycles an allocation for later reuse by Alloc
+func (b *BumpRecycle) Recycle(obj unsafe.Pointer) {
+	b.lock.Lock()
+
+	// Save front of linked list
+	prev := b.recycled
+	// Set new front of linked list to one pointer width in front of allocation ('next' field)
+	b.recycled = unsafe.Pointer(uintptr(obj) - ptrSize)
+	// Set 'next' field to previous value
+	*(*unsafe.Pointer)(b.recycled) = prev
+
+	b.lock.Unlock()
+}
+
+// FreeSpace returns the amount of free space left in the allocator without allocating more memory from main allocator
+func (b *BumpRecycle) FreeSpace() uintptr {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	return b.regionSize - b.freePtr
+}
+
+// *** Bump allocator with recycling (not thread-safe) ***
+
+// FastBumpRecycle allocator is a bump allocator with object recycling. It is not thread safe and is
+// designed to be used in only a single go routine. The allocator can be used for a single object type
+// or multiple, however, the largest possible object must be specified as 'allocSize'.The allocator
+// does not allocate any memory until the first request. Objects can be recycled by returning them,
+// they are not cleared (if that is desired, the caller must do it). Memory is not eligible for
+// garbage collection until all objects in a 'region' are not in use and the next region has been
+// requested.
+type FastBumpRecycle struct {
+	recycled                       unsafe.Pointer
+	allocSize, regionSize, freePtr uintptr
+	region                         []byte
+}
+
+// NewFastBumpRecycle returns a new bump allocator with recycling. 'allocSize' should be equal to the
+// largest object to be requested. 'regionSize' determines the size of each memory region. An error
+// is only returned if allocSize is greater than regionSize
+func NewFastBumpRecycle(allocSize, regionSize uintptr) (*FastBumpRecycle, error) {
+	if allocSize > regionSize {
+		return nil, errors.New("Allocation size must be less than or equal to region size")
+	}
+	// We add an extra pointer to front of every allocation so we can create a linked list of recycled objects
+	return &FastBumpRecycle{
+		allocSize:  allocSize + ptrSize,
+		regionSize: regionSize + ptrSize,
+		freePtr:    regionSize,
+	}, nil
+}
+
+// Alloc allocates memory of the size requested by first checking and using a 'recycle' list and if empty
+// 'bumping' the freePtr to the next position to get an allocation. If there isn't enough memory left
+// in the region, a new memory region is first allocated
+func (b *FastBumpRecycle) Alloc() unsafe.Pointer {
+	// If we have recycled objects, use them first
+	if b.recycled != nil {
+		// Get allocation from front of linked list (stored just past the 'next' pointer)
+		obj := unsafe.Pointer(uintptr(b.recycled) + ptrSize)
+		// The first field in the allocation is actually an unsafe.Pointer pointing at next free allocation
+		next := (*unsafe.Pointer)(b.recycled)
+		// Update the linked list to point point at the next entry (effectively popping top entry)
+		b.recycled = *next
+		// Nil out the value of the next pointer inside allocation now that we are handing this out
+		*next = nil
+
+		return obj
+	}
+
+	// Are we out of memory?
+	if b.allocSize > b.regionSize-b.freePtr {
+		// Allocate memory and zero it - intentionally dumping reference to the old region (eligible for
+		// garbage collection when none of its objects are no longer alive)
+		b.region = make([]byte, b.regionSize)
+		b.freePtr = 0
+	}
+
+	obj := b.region[b.freePtr+ptrSize:]
+	b.freePtr += b.allocSize
+	return unsafe.Pointer(&obj[0])
+}
+
+// Recycle recycles an allocation for later reuse by Alloc
+func (b *FastBumpRecycle) Recycle(obj unsafe.Pointer) {
+	// Save front of linked list
+	prev := b.recycled
+	// Set new front of linked list to one pointer width in front of allocation ('next' field)
+	b.recycled = unsafe.Pointer(uintptr(obj) - ptrSize)
+	// Set 'next' field to previous value
+	*(*unsafe.Pointer)(b.recycled) = prev
+}
+
+// FreeSpace returns the amount of free space left in the allocator without allocating more memory from main allocator
+func (b *FastBumpRecycle) FreeSpace() uintptr {
 	return b.regionSize - b.freePtr
 }
--- alloc_test.go
+++ alloc_test.go
@@ -11,18 +11,24 @@ import (
 
 var (
 	people person
+	ptr    unsafe.Pointer
+
+	recycleProb = 5
 
 	bumpTests = []struct {
-		name      string
-		chunkSize uintptr
+		name        string
+		chunkSize   uintptr
+		recycleProb int
 	}{
-		{"100B", 100}, {"1000B", 1000}, {"1KB", 1000}, {"10KB", 10000}, {"100KB", 100000},
-		{"1MB", 1000000}, {"10MB", 10000000}, {"100MB", 100000000},
+		{"100B", 100, recycleProb}, {"1KB", 1000, recycleProb}, {"10KB", 10000, recycleProb},
+		{"100KB", 100000, recycleProb}, {"1MB", 1000000, recycleProb}, {"10MB", 10000000, recycleProb},
+		{"100MB", 100000000, recycleProb},
 	}
 )
 
 const (
 	personSize = unsafe.Sizeof(people)
+	ptrSize    = unsafe.Sizeof(ptr)
 )
 
 // person is just a bogus struct for testing. Goal was to squeeze in many
@@ -48,7 +54,7 @@ func TestFastBump(t *testing.T) {
 	testBump(t, bump)
 }
 
-func testBump(t *testing.T, bump allocator.BumpAllocator) {
+func testBump(t *testing.T, bump allocator.BasicBump) {
 	t.Run("Alloc", func(t *testing.T) {
 		p := (*person)(bump.Alloc(personSize))
 		if !reflect.DeepEqual(p, new(person)) {
@@ -94,6 +100,98 @@ func TestBumpThreads(t *testing.T) {
 	wg.Wait()
 }
 
+func TestBumpRecycle(t *testing.T) {
+	_, err := allocator.NewBumpRecycle(10, 9)
+	if err == nil {
+		t.Error("No error received, but error expected")
+	}
+	bump, err := allocator.NewBumpRecycle(personSize, ((personSize+ptrSize)*3)+10)
+	if err != nil {
+		t.Error("Error creating bump recycler")
+	}
+	testBumpRecycle(t, bump)
+}
+
+func TestFastBumpRecycle(t *testing.T) {
+	_, err := allocator.NewFastBumpRecycle(10, 9)
+	if err == nil {
+		t.Error("No error received, but error expected")
+	}
+	bump, err := allocator.NewFastBumpRecycle(personSize, ((personSize+ptrSize)*3)+10)
+	if err != nil {
+		t.Error("Error creating bump recycler")
+	}
+	testBumpRecycle(t, bump)
+}
+
+func testBumpRecycle(t *testing.T, bump allocator.RecyclingBump) {
+	t.Run("Alloc/Recycle", func(t *testing.T) {
+		p := (*person)(bump.Alloc())
+		if !reflect.DeepEqual(p, new(person)) {
+			t.Error("Allocation was not zeroed")
+		}
+		p.name = "Teddy"
+
+		p2 := (*person)(bump.Alloc())
+		if !reflect.DeepEqual(p2, new(person)) {
+			t.Error("Allocation was not zeroed")
+		}
+		p2.name = "Tucker"
+
+		bump.Recycle(unsafe.Pointer(p))
+		bump.Recycle(unsafe.Pointer(p2))
+	})
+	t.Run("AllocWithMostlyRecycled", func(t *testing.T) {
+		// We didn't clear our objects before returning so they should still match
+		p := (*person)(bump.Alloc())
+		if !reflect.DeepEqual(p, &person{name: "Tucker"}) {
+			t.Error("Allocation didn't match")
+		}
+		p = (*person)(bump.Alloc())
+		if !reflect.DeepEqual(p, &person{name: "Teddy"}) {
+			t.Error("Allocation didn't match")
+		}
+		// Out of recycled objects, this one should be zeroed
+		p = (*person)(bump.Alloc())
+		if !reflect.DeepEqual(p, new(person)) {
+			t.Error("Allocation was not zeroed")
+		}
+	})
+	t.Run("FreeSpace", func(t *testing.T) {
+		if bump.FreeSpace() != (10 + ptrSize) {
+			t.Errorf("Wrong amount of free space (%d)", bump.FreeSpace())
+		}
+	})
+}
+
+func TestBumpRecycledThreads(t *testing.T) {
+	bump, _ := allocator.NewBumpRecycle(personSize, personSize+10)
+	times := 100
+	var wg sync.WaitGroup
+
+	for i := 0; i < times; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for j := 0; j < times; j++ {
+				p := (*person)(bump.Alloc())
+				if !reflect.DeepEqual(p, new(person)) {
+					t.Error("Allocation was not zeroed")
+				}
+				if j%recycleProb == 0 {
+					bump.Recycle(unsafe.Pointer(p))
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+}
+
+// *** Benchmarks ***
+
 func BenchmarkBump(b *testing.B) {
 	for _, test := range bumpTests {
 		b.Run(test.name, func(b *testing.B) {
@@ -116,6 +214,42 @@ func BenchmarkFastBump(b *testing.B) {
 
 			for n := 0; n < b.N; n++ {
 				*p = (*person)(bump.Alloc(personSize))
+				p = &(*p).partner
+			}
+		})
+	}
+}
+
+func BenchmarkBumpRecycle(b *testing.B) {
+	for _, test := range bumpTests {
+		b.Run(test.name, func(b *testing.B) {
+			bump, _ := allocator.NewBumpRecycle(personSize, test.chunkSize)
+			p := &people.partner
+
+			for n := 0; n < b.N; n++ {
+				*p = (*person)(bump.Alloc())
+				if n%test.recycleProb == 0 {
+					bump.Recycle(unsafe.Pointer(*p))
+					continue
+				}
+				p = &(*p).partner
+			}
+		})
+	}
+}
+
+func BenchmarkFastBumpRecycle(b *testing.B) {
+	for _, test := range bumpTests {
+		b.Run(test.name, func(b *testing.B) {
+			bump, _ := allocator.NewFastBumpRecycle(personSize, test.chunkSize)
+			p := &people.partner
+
+			for n := 0; n < b.N; n++ {
+				*p = (*person)(bump.Alloc())
+				if n%test.recycleProb == 0 {
+					bump.Recycle(unsafe.Pointer(*p))
+					continue
+				}
 				p = &(*p).partner
 			}
 		})
